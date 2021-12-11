2a517105377069be0e16dd74eb548f7e660f450c
Sergey Eremenko <se@develup.pro>
2021-12-11 13:06:39 +0200

    first project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ReactNastavnikys
+
+## Методички
+* Шпаргалка ES6 - [ссылка](https://docs.google.com/document/d/1y3cIC_yyq7MzPszybyTZm0P6crHud8PuSPXRAw2IOIo/edit?usp=sharing)
+* Настройка среды разработки - [ссылка](https://docs.google.com/document/d/1zIRR2FaFLp2UUBXF81RHDnEyZAbBtkgWg5ZgIFrH4Bg/edit?usp=sharing)
+
+## Первый запуск
+* Для установки всех модулей запустите в командной строке или терминале `yarn` или `npm i`
+* Для старта локального сервера из папки `src` запустите `npm start`
+* Для старта локального сервера из папки `lesson1` запустите `npm run path lesson1`
+* Для создания готового билда из папки `src` запустите `npm run build`
+* Для создания готового билда из папки `lesson1` запустите `npm run buildPath lesson1`
+* Все вышеперечисленные команды были созданы в файле `package.json`
---
+++ webpack.config.js
@@ -0,0 +1,133 @@
+const path = require('path'),
+  minimist = require('minimist'),
+  HTMLplugin = require('html-webpack-plugin'),
+  CopyPlugin = require('copy-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+  OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+  args = minimist(process.argv, {
+    alias: { name: 'n', }
+  }),
+  pathName = args.name || 'src',
+  buildName = args.name ? `build-${args.name}` : 'dist',
+  isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  // куда смотреть, точка входа
+  entry: {
+    main: path.resolve(__dirname, pathName, 'index.js'),
+  },
+  // куда собирать билд
+  output: {
+    path: path.resolve(__dirname, buildName),
+    filename: 'bundle.[hash].js',
+    publicPath: '/',
+    clean: true,
+  },
+  // настройка локального сервера
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, buildName),
+      watch: true,
+    },
+    watchFiles: [`${pathName}/**/*`],
+    //настройка прокси json-server
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        pathRewrite: {'^/api' : ''},
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  // настройка всех загрузчиков (какими лоадерами обрабатывать файлы)
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // регулярное выражение - совпадения имени файла
+        exclude: /node_modules/, // регулярное выражение - игнорировать если в имени есть это
+        use: {
+          loader: 'babel-loader' // какой лоадер использовать
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  // алиасы для коротких путей к файлам
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      assets: path.resolve(__dirname, pathName, 'assets'),
+      images: path.resolve(__dirname, pathName, 'assets', 'images'),
+      styles: path.resolve(__dirname, pathName, 'styles'),
+      pages: path.resolve(__dirname, pathName, 'pages'),
+      hoc: path.resolve(__dirname, pathName, 'hoc'),
+      containers: path.resolve(__dirname, pathName, 'containers'),
+      components: path.resolve(__dirname, pathName, 'components'),
+      contexts: path.resolve(__dirname, pathName, 'contexts'),
+      store: path.resolve(__dirname, pathName, 'store'),
+      actions: path.resolve(__dirname, pathName, 'store', 'actions'),
+      reducers: path.resolve(__dirname, pathName, 'store', 'reducers'),
+      types: path.resolve(__dirname, pathName, 'types'),
+      constants: path.resolve(__dirname, pathName, 'constants'),
+      configs: path.resolve(__dirname, pathName, 'configs'),
+      services: path.resolve(__dirname, pathName, 'services'),
+      navigation: path.resolve(__dirname, pathName, 'navigation'),
+      '@': path.resolve(__dirname, pathName),
+    }
+  },
+  // оптимизация, минификация билда
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  // подключение плагинов
+  plugins: [
+    new HTMLplugin({
+      template: path.resolve(__dirname, pathName, 'index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+    // копировать любую директорию
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, pathName, 'favicons'),
+    //       to: path.join(__dirname, buildName),
+    //     },
+    //   ],
+    // }),
+  ],
+};
